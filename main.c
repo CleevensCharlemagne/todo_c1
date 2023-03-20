625dcbd115aaf9a978719b8bd48ea3abac7da3a7
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-19 23:07:13 -0400

    update

--- main.c
+++ main.c
@@ -149,3 +149,92 @@ void seetodo()
 	system("pause");
 }
 
+// Function to insert a node todo
+void createtodo()
+{
+	// Choose choice from user
+	char c;
+
+	// Pointers to node
+	todo *add, *temp;
+	system("cls");
+
+	// Infinite loop which will
+	// break if "n" is pressed
+	while (1) {
+
+		printf("\nWant to add new ToDo ??"
+			+ " Press 'y' for Yes and 'n' "
+			+ " for No :-)\n\t\t");
+		fflush(stdin);
+
+		// Input from user
+		scanf("%c", &c);
+
+		if (c == 'n')
+			break;
+		else {
+
+			// If start node is NULL
+			if (start == NULL) {
+
+				// Dynamically allocating
+				// memory to the newly
+				// created node
+				add = (todo*)calloc(1, sizeof(todo));
+
+				// Using add pointer to
+				// create linked list
+				start = add;
+				printf("\nType it.....\n");
+
+				// Input from user
+				fflush(stdin);
+				gets(add->buffer);
+
+				// As first input so
+				// count is 1
+				add->count = 1;
+
+				// As first node so
+				// start's next is NULL
+				start->next = NULL;
+			}
+			else {
+				temp = (todo*)calloc(1, sizeof(todo));
+				printf("\nType it.....\n");
+				fflush(stdin);
+				gets(temp->buffer);
+
+				// Insertion is at last
+				// so pointer part is NULL
+				temp->next = NULL;
+
+				// add is now pointing
+				// newly created node
+				add->next = temp;
+				add = add->next;
+			}
+
+			// Using the concept of
+			// insertion at the end,
+			// adding a todo
+
+			// Calling function to adjust
+			// the count variable
+			adjustcount();
+		}
+	}
+}
+
+// Function to delete the todo
+void deletetodo()
+{
+	system("cls");
+
+	// To get the numbering of the
+	// todo to be deleted
+	int x;
+
+
+}
