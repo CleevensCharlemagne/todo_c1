0b1a89c2f2dc5422de57cc4f1edc0b458549ee44
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-19 23:00:57 -0400

    update

--- main.c
+++ main.c
@@ -5,3 +5,16 @@
 // Renaming structure to avoid the
 // repetitive use of struct keyword
 typedef struct ToDo todo;
+
+// Declaration of structure
+struct ToDo {
+	// char array as data part
+	char buffer[101];
+
+	// Pointer part to access addresses
+	todo* next;
+
+	// Count variable for counting
+	// the number of nodes
+	int count;
+};
